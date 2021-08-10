75257d2396423caa22df896d0c2b72b8a7591a58
fernandofrantz <fernandofrantz12@gmail.com>
2021-08-10 18:00:17 -0300

    refatorar

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,15 @@
 let divtest = document.getElementById('tela-test');
-
 function createDiv (){
     let div = document.createElement('div');
     return div;
 }
 
 function createBoard(){
-
     for (let i = 1; i <= 7; i++){
         let divColuna = new createDiv;
         divtest.appendChild(divColuna);
         divColuna.setAttribute('id', `${i}`);
         divColuna.classList.add('coluna');
-    
         for (let j = 1; j <= 6; j++){
             let divLinha = new createDiv;
             divColuna.appendChild(divLinha);
@@ -22,20 +19,16 @@ function createBoard(){
     }   
 }
 
-function playGame(){
+function playGame() {
     createBoard();
-
     const board = document.querySelectorAll('.coluna');
     const boardArr = [...board];
-    
-    boardArr.forEach((button)=>{
-        
-        button.addEventListener('click', function(){
+    boardArr.forEach((button) => {
+        button.addEventListener('click', function () {
             let checkColumn = button.id
-          
-            for (let i = 6; i >= 1; i--){
+            for (let i = 6; i >= 1; i--) {
                 let divCheck = document.getElementById(`${checkColumn}${i}`)
-                if (divCheck.childElementCount === 0){
+                if (divCheck.childElementCount === 0) {
                     let disc = new createDiv;
                     disc.classList.add('disc');
                     divCheck.appendChild(disc);
@@ -48,5 +41,5 @@ function playGame(){
         })
     })
 }
-
 playGame()
+
