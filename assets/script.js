e2eed815bddb541c5af4e1d71f432b1db07446a4
Arthur Sarmento <arthurbs17@gmail.com>
2021-08-10 17:31:36 -0300

    Função de add disco

--- assets/script.js
+++ assets/script.js
@@ -13,16 +13,45 @@ function createBoard(){
         divColuna.setAttribute('id', `${i}`);
         divColuna.classList.add('coluna');
     
-        for (let j = 1; j <= 7; j++){
+        for (let j = 1; j <= 6; j++){
             let divLinha = new createDiv;
             divColuna.appendChild(divLinha);
             divLinha.setAttribute('id', `${i}${j}`);
             divLinha.classList.add('linha');
-            if(j === 7){
-                divLinha.classList.add('ocupado')
-            }
         }
     }
+
+    
 }
 
-createBoard();
+
+function playGame(){
+    createBoard();
+
+    const board = document.querySelectorAll('.coluna');
+    const boardArr = [...board];
+    
+    boardArr.forEach((button)=>{
+        
+        button.addEventListener('click', function(){
+            let checkColumn = button.id
+          
+
+            for (let i = 6; i >= 1; i--){
+                let divCheck = document.getElementById(`${checkColumn}${i}`)
+                if (divCheck.childElementCount === 0){
+                    let disc = new createDiv;
+                    disc.classList.add('disc');
+                    divCheck.appendChild(disc);
+                    console.log(divCheck)
+                    break
+                }
+            }
+            
+            console.log(checkColumn);
+            console.log(button);
+        })
+    })
+}
+
+playGame()
