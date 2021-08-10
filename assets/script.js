9727208e0f2f93dd78395aa151028c9c500133e1
Arthur Sarmento <arthurbs17@gmail.com>
2021-08-10 14:31:29 -0300

    Nova linha add

--- assets/script.js
+++ assets/script.js
@@ -13,11 +13,14 @@ function createBoard(){
         divColuna.setAttribute('id', `${i}`);
         divColuna.classList.add('coluna');
     
-        for (let j = 1; j <= 6; j++){
+        for (let j = 1; j <= 7; j++){
             let divLinha = new createDiv;
             divColuna.appendChild(divLinha);
             divLinha.setAttribute('id', `${i}${j}`);
             divLinha.classList.add('linha');
+            if(j === 7){
+                divLinha.classList.add('ocupado')
+            }
         }
     }
 }
