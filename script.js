3adb76f016b3c1286119ec0d90449145b9db70e5
Arthur Sarmento <arthurbs17@gmail.com>
2021-08-10 11:30:59 -0300

    Board JS criado

---
+++ script.js
@@ -0,0 +1,25 @@
+let divtest = document.getElementById('tela-test');
+
+function createDiv (){
+    let div = document.createElement('div');
+    return div;
+}
+
+function createBoard(){
+
+    for (let i = 1; i <= 7; i++){
+        let divColuna = new createDiv;
+        divtest.appendChild(divColuna);
+        divColuna.setAttribute('id', `${i}`);
+        divColuna.classList.add('coluna');
+    
+        for (let j = 1; j <= 6; j++){
+            let divLinha = new createDiv;
+            divColuna.appendChild(divLinha);
+            divLinha.setAttribute('id', `${i}${j}`);
+            divLinha.classList.add('linha');
+        }
+    }
+}
+
+createBoard();
